9f919c3986c0438517862f87e596e2948023577e
pearlmilk-coding <pearlomilk0803@gmail.com>
2021-10-24 22:00:32 +0800

    Site updated: 2021-10-24 22:00:31

--- js/music.js
+++ js/music.js
@@ -7,13 +7,13 @@ const ap = new APlayer({
       {
         name: "Hero-cash cash",
         artist: 'Tokyo Revenger',
-        url: 'https://whyp.it/t/hero-cash-cash-tokyo-revengers-amv-YRQBX',
+        url: 'https://drive.google.com/file/d/1xCJbom4L9CJ504MG9MliyPHFF3TmRKU3/view?usp=sharing',
         cover: 'https://p3.music.126.net/AAq1qOhfyrClGK1mg3mGYQ==/18776360067593424.jpg',
       },
       {
         name: '风筝误',
         artist: '刘珂矣',
-        url: 'https://www.youtube.com/watch?v=NrEozdiPOt0',
+        url: 'https://music.163.com/song/media/outer/url?id=1434570079.mp3',
         cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
       }
     ]
