3b3804f287fd8c9fe47bf2e57d7ff1719394aae1
pearlmilk-coding <pearlomilk0803@gmail.com>
2021-10-26 13:21:08 +0800

    Site updated: 2021-10-26 13:21:06

--- js/music.js
+++ js/music.js
@@ -8,14 +8,14 @@ const ap = new APlayer({
         name: "Hero-cash cash",
         artist: 'Tokyo Revenger',
         url: 'https://drive.google.com/uc?export=download&id=1xCJbom4L9CJ504MG9MliyPHFF3TmRKU3',
-        cover: 'https://p3.music.126.net/AAq1qOhfyrClGK1mg3mGYQ==/18776360067593424.jpg',
-        lrc: "/music/lyrics/Hero-cash.lrc"
+        cover: 'https://drive.google.com/uc?export=download&id=1iHAhGd3wpY1yXxBC0PHBPAEbrDDGBllI',
+        lrc: "/js/music/lyrics/Hero - Cash Cash Tokyo Revengers {AMV}.lrc"
       },
       {
-        name: '风筝误',
-        artist: '刘珂矣',
-        url: 'https://music.163.com/song/media/outer/url?id=1434570079.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+        /*name: '',
+        artist: '',
+        url: '',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.', */
       }
     ]
 });
