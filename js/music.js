d26eb7f1d0d91c69d22bcbf9d5169e133ba2821e
pearlmilk-coding <pearlomilk0803@gmail.com>
2021-11-06 07:00:50 +0800

    Site updated: 2021-11-06 07:00:49

--- js/music.js
+++ js/music.js
@@ -14,17 +14,29 @@ const ap = new APlayer({
         
       },
       {
+        name: "I Like Something Just like This",
+        artist: "The Chainsmokers & Coldplay",
+        url: "https://drive.google.com/uc?export=download&id=17QOrarK7J37TcXeNPMqABnli1ZzWJRmz",
+        cover:"https://drive.google.com/uc?export=download&id=1lYbAYWejT-OierkdJubPTGmbNzKjQCQN"
+      },
+      {
+        name:"你的答案",
+        artist:"阿冗",
+        url:"https://drive.google.com/uc?export=download&id=12LiWE09smjhXtv1hwBEjLh18luMYKkiV",
+        cover:"https://drive.google.com/uc?export=download&id=1XZRh0_zQw9T1tOn4gPCrxblwZeeO8pc7"
+      },
+      {
         name: '人聲-所念皆星河',
         artist: '房東的貓',
         url: 'https://drive.google.com/uc?export=download&id=1F44TepMAx9dyiY65KXHIBoMZWbRpRH3K',
         cover: 'https://drive.google.com/uc?export=download&id=1bEtsHmISmKQ_RiWawbHZLjFzmvb0jwkc', 
+        
       },
       {
         name: "所念皆星河",
         artist: "純音樂",
         url: "https://drive.google.com/uc?export=download&id=1rAy5kIhbvrTDffuecSRWN5hYQ1-1YcTE",
         cover: "https://drive.google.com/uc?export=download&id=1rSXxEgmMKQltPnj4nScA1JTtTWIgDVaB",
-        
       }
 
     ]
