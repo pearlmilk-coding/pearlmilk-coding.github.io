a0484c1d3cf850c957d2c5906f608668a609f1be
pearlmilk-coding <pearlomilk0803@gmail.com>
2021-11-06 19:52:08 +0800

    Site updated: 2021-11-06 19:52:05

--- js/pmt.js
+++ js/pmt.js
@@ -1,5 +1,4 @@
 /*UP 醬 */
-
 /*添加图片top*/
 var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
 /*添加到返回顶部按钮下*/
